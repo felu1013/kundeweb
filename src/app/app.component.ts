8677e9ad6fe9421f9fab14ccfea6ebe0629fee2b
felu1013 <102032721+felu1013@users.noreply.github.com>
2022-05-04 12:44:25 +0200

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2018 - present Juergen Zimmermann, Hochschule Karlsruhe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 // "core" enthaelt Funktionalitaet, damit die Webanwendung im Browser laeuft
 import { Component } from '@angular/core';
 // statt console.log(...)
@@ -65,14 +48,14 @@ import log from 'loglevel';
     //       </hs-header>
     //       <hs-main>
     //           <router-outlet>
-    //               <hs-suche-buecher>
+    //               <hs-suche-fahrraeder>
     //                   <hs-suchformular>
     //                       ...
     //                   </hs-suchformular>
     //                   <hs-suchergebnis>
     //                       ...
     //                   </hs-suchergebnis>
-    //               </hs-suche-buecher>
+    //               </hs-suche-fahrraeder>
     //           <router-outlet>
     //       </hs-main>
     //   </hs-root>
